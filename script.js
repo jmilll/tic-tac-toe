13cce0b75ba6d232074c8d6e042d92b71ad750a8
jmilll <jmillgames@gmail.com>
2020-12-11 16:16:44 -0500

    the game works fully

--- script.js
+++ script.js
@@ -32,23 +32,47 @@ document.querySelector('.gameboard').addEventListener('click', (e) => {
     if (!e.target) { return; }
     if (e.target.matches('.tile')) {
         //this.textContent = 'x'
-        console.log('tile')
-        // changes the div to have text/mark
-        e.target.innerText = 'x'
+        //console.log('tile')
+
+
         //console.log(e.target.getAttribute('data-value'))
         let v = e.target.getAttribute('data-value');
-        let player = 'playerOne';
+
+
+        //let player = 'playerOne';
+
+        let player = controller.getTurn();
+
+        // changes the div to have text/mark 
+        //e.target.innerText = 'x'
+
         //gameBoard.mark(v) //before adding player
         gameBoard.mark(player, v)
+        controller.toggleTurn();
+        gameBoard.renderBoard()
+        gameBoard.checkWinner()
     }
 });
+
+/*
+// --for mark function reference --
+    const mark = (player, value) => {
+        const boardObj = board[value].mark
+        if (!boardObj === ''){return};
+        if (boardObj === '' && player === 'p1') {
+            return board[value].mark = 'x';  
+        } else {
+            return board[value].mark = 'o'; 
+        }
+    }
+*/
 
 const gameBoard = (function() {
     'use strict';
     
-    const tile = {
-        mark: ''
-    }
+    //const tile = {
+    //    mark: ''
+    //}
     const board = [];
 
     const getBoard = () => {
@@ -111,6 +135,7 @@ const gameBoard = (function() {
         createTiles();
         //takes all tiles renders from board array
         const tiles = document.querySelectorAll('.tile');
+        //erase current board to populate from board
         tiles.forEach(tile => document.querySelector('.gameboard').removeChild(tile));
         for (let j = 0; j < board.length; j++) {
             //create dom element for each object in array
@@ -129,58 +154,129 @@ const gameBoard = (function() {
     */
     const mark = (player, value) => {
         const boardObj = board[value].mark
+        // !boardobj doesnt stop o from overwriting x for some reason
         if (!boardObj === ''){return};
-        if (boardObj === '' && player === 'playerOne') {
+        if (boardObj === '' && player === 'p1') {
             return board[value].mark = 'x';  
-        } else {
+        } else if (boardObj === '' && player === 'p2') {
             return board[value].mark = 'o'; 
         }
+    }
+
+    const winCon = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    
+    //find a winner. Loops thru wincons and applys them, checking if marks match
+    const checkWinner = () => {
+    
+        let xx = 0;
+        let yy = 0;
+        let zz = 0;
+        
+        let winner = false;
+
+        for(let i = 0; i < winCon.length; i++) {
+            //const board = gameBoard.getBoard(); //to work outside gameboard method
+            const turns = controller.turnNumber();
+            xx = winCon[i][0]
+            yy = winCon[i][1]
+            zz = winCon[i][2]
+    
+            //console.log( xx , yy , zz)
+        
+            if ( board[xx].mark !== '' && board[xx].mark == board[yy].mark && board[yy].mark == board[zz].mark) {
+                console.log('winner = ', board[xx].mark )
+                winner = true;
+                break;
+            } else if ( turns === 9 && winner !== true) {
+                console.log('tie game')
+                break;
+            }
+        }    
     }
 
     return {
         getBoard,
         renderBoard,
         mark,
+        checkWinner,
     }
 })();
 gameBoard.renderBoard()
 
 
-const winCon = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
+const controller = (() => {
+    'use strict'
 
-//find a winner. Loops thru wincons and applys them, checking if marks match
-function checkWinner() {
-    let xx = 0;
-    let yy = 0;
-    let zz = 0;
 
-    for(let i = 0; i < winCon.length; i++) {
-
-        xx = winCon[i][0]
-        yy = winCon[i][1]
-        zz = winCon[i][2]
-
-        //console.log( xx , yy , zz)
+    const playerFactory = (player, name, mark) => {
+        //const sayHello = () => console.log('hello!');
+        return { 
+            player,
+            name, 
+            mark, 
+            //sayHello 
+        };
+    };
+      
+    const playerOne = playerFactory('p1', 'Human', 'x');
+    const playerTwo = playerFactory('p2', 'CPU', 'o');
+      
     
-        if ( board[xx].mark !== '' && board[xx].mark == board[yy].mark && board[yy].mark == board[zz].mark) {
-            console.log('winner = ', board[xx].mark )
-            break;
-        }
-    }    
-}
+    const getPlayers = () => {
+        return {playerOne, playerTwo}
+    }
+
+    let turn = playerOne;
+    let turns = 0;
+
+    const getTurn = () => {
+        return turn.player;
+    }
+
+    const turnNumber = () => {
+        return turns;
+    }
+
+    const toggleTurn = () => {
+        //console.log(turn);
+        turn !== playerTwo ? turn = playerTwo : turn = playerOne;
+        turns++;
+        //console.log(turn.name);
+
+        return turn.mark;
+    }
+
+    return {
+        playerFactory,
+        getPlayers,
+        toggleTurn,
+        getTurn,
+        turnNumber,
+    }
+})();
+
+
+
+
+
 
 /*
+        if (turn === playerOne) {
+            turn = playerTwo;
+        } else {
+            turn = playerOne
+        }
 //trying to find the mark value pulling from gameboard
 gameBoard.getBoard().forEach(function(item, index, array) {
-    console.log(item.mark, index)
+    console.log(index, item.mark)
 })
 */
